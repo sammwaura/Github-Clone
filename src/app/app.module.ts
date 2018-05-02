4f7d462496ca02c7d5ea49bc9c1f28534f170a2a
sammwaura <meshsami27@gmail.com>
2018-05-02 09:04:02 +0300

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import {AppService} from './app.service';
 
 import { AppComponent } from './app.component';
 import { GithubCloneComponent } from './github-clone/github-clone.component';
 
+const appRoutes: Routes = [
+  {path:'githubclone', component: GithubCloneComponent}
+
+]
 
 @NgModule({
   declarations: [
@@ -19,7 +24,8 @@ import { GithubCloneComponent } from './github-clone/github-clone.component';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
